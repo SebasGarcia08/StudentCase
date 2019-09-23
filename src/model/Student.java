1c3642cae81397fefb9000cde26cfd79d2a28e59
Sebastián García Acosta <segaracos@outlook.com>
2019-09-22 20:29:12 -0500

    There is lacking organizing the classpath

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,5 @@
 package model;
+// Little lazy trick to avoid typing System.blablabla everytime xd.
 import static java.lang.System.out;
 
 public class Student{
@@ -8,6 +9,11 @@ public class Student{
     public final char RETIRED = 'r';
     public final char GRADUATE = 'g';
     public final int MAX_NUM_COURSES = 5;
+
+    // Although in the workshop it is ordered to add this as an attribute, 
+    // i will add it just as a property since it cannot be established, but depends on 
+    // the average grade and the state of the student.
+    // (I already talked about this with the teacher and said there is no problem).
     public boolean isInAcademicTest = isInAcademicTest();
 
     // Atributes
@@ -24,6 +30,7 @@ public class Student{
       this.courses = new Course[MAX_NUM_COURSES];  
     }
 
+    // Overloaded constructor.
     public Student() {
       this.id = "";
       this.name = "";
@@ -36,7 +43,7 @@ public class Student{
       boolean courseExists = false;
       int num_available_courses=0;
 
-        // Validar que el curso no exista.
+        // Validate the course hasn't been registered yet.
         for (Course course : courses) {
             if (!(course == null)) {
                 if (course.getId().equals(id)) {
@@ -45,28 +52,34 @@ public class Student{
             } 
         }
   
-        // Validar cuántos cursos están disponibles para matricular.
-        for (Course course : courses) {
-            if(course == null) {
-                num_available_courses++;
-            }
-        }
+        // Validate the number of courses available to register.
+        num_available_courses = MAX_NUM_COURSES - getNumberOfCoursesEnrolled();
         
        out.println("\nYOU CAN ENROLL " + (num_available_courses-1)+ " MORE COURSES. ");
        out.print((courseExists) ? "\nCOURSE ALREADY ENROLLED " : " ");
 
         if(!courseExists && num_available_courses > 0){
           //out.println("VALID CONDITIONS");
-         out.print((grade >= 1) ? "" : "\nSince minimal final grade is 1, then switching grade to 1...\n");
-          double final_grade = (grade >= 1) ? grade : 1;
-          int idx = courses.length - num_available_courses;
+          out.print((grade >= 1) ? "" : "\nSince minimal final grade is "+ Course.MIN_GRADE + ", then overwriting it...\n");
+          double final_grade = (grade >= 1) ? grade : Course.MIN_GRADE;
+          int idx = MAX_NUM_COURSES - num_available_courses;
           for(int i = idx; i < (idx + 1); i++){
             courses[i] = new Course(validable, id, name, num_credits, final_grade);
           }
-         out.print( name + " ENROLLED SUCCESFULLY.");
+          out.print(name.toUpperCase() + " ENROLLED SUCCESFULLY.\n");
         } else {
          out.println("UNVALID CONDITIONS TO ENROLL COURSE WITH ID " + id);
         }
+    }
+
+    public void unregisterCourse(String id) {
+      for(int i = 0; i < courses.length; i++){
+        if(courses[i] != null && courses[i].getId().equals(id)){
+          out.print("[Unregistering " + courses[i].getName()+ "...");
+          this.courses[i] = null;
+          out.print(" SUCCESFULLY unregistered.]\n");
+        }
+      }
     }
 
     public double calculateAverageGrade(){
@@ -86,6 +99,14 @@ public class Student{
 
     public boolean isInAcademicTest(){
       return (this.calculateAverageGrade() < 3.25 && this.state == ACTIVE);
+    }
+
+    public int getNumberOfCoursesEnrolled(){
+      int num_enrolled_courses = 0;
+      for(Course course : courses){
+        num_enrolled_courses += (course != null) ? 1 : 0;
+      }
+      return num_enrolled_courses;
     }
 
    // Setters
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 import java.util.Scanner;
 import model.*;
+// Little lazy trick to avoid typing System.blablabla everytime xd.
 import static java.lang.System.out;
 
 public class Main {
@@ -16,39 +17,48 @@ public class Main {
 
   public static void main(String[] args) {
       Main program = new Main();
+      
       program.enrollStudent();
+
       if(student.getState() == student.ACTIVE){
-          program.enrollCourse();
-          if(student.getCourses()[0] != null){
-            program.showLogs();
+          program.enrollCourses();
+          out.print("Do you want to unregister courses of this student? [y/n]: ");
+          char unregister_y_n = sc_str.nextLine().charAt(0);
+          boolean unregister = (unregister_y_n == 'y');
+      
+          if(unregister) {
+            program.unregisterCourses();
           }
         }
-      // out.println("Do you want to unenroll these students");
+        
+      if(student.getNumberOfCoursesEnrolled() > 0){
+          program.showLogs();
+      }
   }
 
-public void enrollStudent(){
-    out.println("\nIngrese los datos del estudiante a registrar: \n");
+  public void enrollStudent(){
+    out.println("\nPlease type in the information requested to register the student: \n");
     
-    out.print("Código: ");
+    out.print("Id: ");
     String id = sc_str.nextLine();
     student.setId(id);
 
-    out.print("Nombre del estudiante: ");
+    out.print("Student's name: ");
     String name = sc_str.nextLine();
     student.setName(name);
 
-    out.print("Apellidos: ");
+    out.print("Last name: ");
     String last_name = sc_str.nextLine();
     student.setLastName(last_name);
 
-    out.print("Estado: [a: Activo / r: Retirado / g: Graduado]: ");
+    out.print("State: [a: ACTIVE / r: RETIRED / g: GRADUATE]: ");
     char state = sc_str.nextLine().charAt(0);
     student.setState(state);
 
     out.println("\nStudent '" + student.getName() + " " + student.getLastName() + "' with id '" + student.getId() + "' succesfully enrolled.");
-}
+  }
 
-  public void enrollCourse(){
+  public void enrollCourses(){
     out.print("\nType the number of courses you want to enroll for the student: ");
     int num_courses = sc_int.nextInt();
     if(num_courses <= student.MAX_NUM_COURSES)
@@ -76,15 +86,31 @@ public void enrollStudent(){
       out.println("You CANNOT enroll more than " + student.MAX_NUM_COURSES + " courses.");
   }
 
+  public void unregisterCourses(){
+    out.print("\nHow many courses do you want to unregister?: ");
+    int num_courses = sc_int.nextInt();
+    
+    // Validate the number of courses registered.    
+    if(num_courses <= student.getNumberOfCoursesEnrolled()){
+      for(int i=0; i < num_courses; i++){
+        out.print("Type the id of the course you want to unregister: ");
+        String id = sc_str.nextLine();
+        student.unregisterCourse(id);
+      }
+    } else {
+      out.println("You CANNOT unregister more courses than the ones already enrolled.");
+    }
+  }
+
   public void showLogs(){
     out.println("\nEnrolled courses: \n");
     for(Course course : student.getCourses()){
         try {
-            out.println(course.getId());
-            out.println(course.getName());
-            out.println(course.getGrade());
-            out.println(course.getNumCredits());
-            out.println(course.getValidable());
+            out.println(" Id: " + course.getId());
+            out.println(" Name: " + course.getName());
+            out.println(" Grade: " + course.getGrade());
+            out.println(" Number of credits:  "+course.getNumCredits());
+            out.println(" Is validable: " + ( (course.getValidable()) ? "YES": "NO"));
             out.println();    
         } catch(Exception e) {
           continue;
